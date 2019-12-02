c75dd6aca7bb8679342d49104fce36570e1ce31a
Ciaran Bent [K00221230] <k00221230@student.lit.ie>
2019-12-02 16:25:58 +0000

    Added button listeners; Added quit function;

---
+++ UGame/Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuButton : MonoBehaviour {
+    public MainMenuOption menuOption;
+    
+    private Button _button;
+    private MngrMainMenu _manager;
+
+    private void Start()
+    {
+        _button = GetComponent<Button>();
+        Debug.Log("Initialising new Button: " + _button.name);
+        
+        _button.onClick.AddListener(LaunchSelection);
+    }
+
+    private void LaunchSelection() {
+        Debug.Log("Button " + _button.name + " was pressed.");
+        switch (menuOption) {
+            case MainMenuOption.START_GAME:
+                break;
+            case MainMenuOption.SETTINGS:
+                break;
+            case MainMenuOption.QUIT:
+                Application.Quit();
+                break;
+        }
+    }
+    
+    private void DebugButton() {
+        Debug.Log("Button " + _button.name + " was pressed.");
+    }
+}
--- UGame/Assets/Scripts/MngrMainMenu.cs
+++ UGame/Assets/Scripts/MngrMainMenu.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class MngrMainMenu : MonoBehaviour
-{
+public enum MainMenuOption {
+    START_GAME,
+    SETTINGS,
+    QUIT
+}
+
+public class MngrMainMenu : MonoBehaviour {
+    
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         
     }
 }
